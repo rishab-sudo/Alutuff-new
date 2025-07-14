bf7153db83d82ebb0db6d592bf199597c6e43c6c
rishab-sudo <rishabdutt4@gmail.com>
2025-07-14 16:08:55 +0530

    13july25

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -86,7 +86,7 @@ const Navbar = () => {
           </li>
 
           <li><a href="/product" onClick={toggleMenu}>Product</a></li>
-          <li><a href="/projects" onClick={toggleMenu}>projects</a></li>
+          <li><a href="/projects" onClick={toggleMenu}>Projects</a></li>
                     <li><a href="/career" onClick={toggleMenu}>Career</a></li>
           {/* Mobile More Dropdown */}
           <li className="side-dropdown">

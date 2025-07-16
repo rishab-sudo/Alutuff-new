9e71134ad078715438fa9ded41d344ddb812b144
rishab-sudo <rishabdutt4@gmail.com>
2025-07-16 13:36:47 +0530

    15july25

--- src/components/StayWithUs.js
+++ src/components/StayWithUs.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import BackgroundImage from '../assets/img-2.jpeg'
+import { Link } from 'react-router-dom'
 import "./StayWithUs.css"
 
 const StayWithUs = () => {
   return (
  <div className="stay-with-section" style={{ backgroundImage: `url(${BackgroundImage})` }}>
       <div className="stay-with-content">
-        <h1>Welcome to Our Site</h1>
-        <h2>Your Success Starts Here</h2>
+       
+        <h1>Step Into Quality, Step Into Success</h1>
+        <Link to="/contact">
           <button className='stay-button'>
 Let's Talk
   <span className="stay-button-circle">
     <i className="fa fa-arrow-up"></i>
   </span>
 </button>
+</Link>
       </div>
     </div>
   )

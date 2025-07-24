a74def93751c9ecefb35eec5bc90376c0bdb0643
rishab-sudo <rishabdutt4@gmail.com>
2025-07-24 16:45:04 +0530

    24july25

--- src/components/LinkTree.js
+++ src/components/LinkTree.js
@@ -20,8 +20,13 @@ const LinkTree = () => {
               <li><a href="https://www.facebook.com/profile.php?id=61578129710470" target="_blank" rel="noreferrer">Facebook</a></li>
               <li><a href="https://www.instagram.com/alutuff.panels/" target="_blank" rel="noreferrer">Instagram</a></li>
               <li><a href="https://wa.me/916396854974" target="_blank" rel="noreferrer">WhatsApp</a></li>
-              {/* <li><a href="https://www.linkedin.com/company/alutuff/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-              <li><a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a></li> */}
+              <li><a href="https://www.linkedin.com/company/alutuff/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+              <li ><a href="/projects" target="_blank" rel="noreferrer">Projects Portfolio</a></li>
+            <li><a href="mailto:sales@alutuff.in">Email</a></li>  
+              <li><a href="tel:+916396854974">Call Us</a></li>
+
+
+             {/*  <li><a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a></li> */}
 
             </ul>
           </div>
